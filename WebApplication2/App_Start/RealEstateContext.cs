b78896915dc288e4c5b9718e90a2994d0818a15c
Filipp Vynnykov <ynov07@rambler.ru>
2019-05-03 11:50:55 +0300

    Add project files.

---
+++ WebApplication2/App_Start/RealEstateContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Properties;
+
+namespace WebApplication2.App_Start
+{
+    public class RealEstateContext
+    {
+        public IMongoDatabase MongoDatabase;
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+            MongoDatabase = client.GetDatabase(Settings.Default.RealEstateDatabaseName);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using WebApplication2.Properties;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return Json(MongoDatabase.Client.ListDatabaseNames().ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
